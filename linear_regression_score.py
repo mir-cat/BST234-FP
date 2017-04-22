4c6d2d4c08dd1d249dbc863a3e8a45be0ca53303
mir-cat <mir.henglin@gmail.com>
2017-04-22 08:42:18 -0400

    Linear regression p_value bug
    Fixed bug preventing correct p_value calculation

--- linear_regression_score.py
+++ linear_regression_score.py
@@ -4,7 +4,6 @@ import statsmodels.api as sm
 import time
 from logistic_regression_score import import_data, permute_column
 
-
 def solve_linear_regression(X, y):
 
     # X : pandas dataframe
@@ -14,14 +13,12 @@ def solve_linear_regression(X, y):
 
     return beta
 
-
 if __name__ =='__main__':
     KEY = 'X_1'
     RESPONSE = 'Y'
-    NUM_PERMUTATIONS = 1000
+    NUM_PERMUTATIONS = 10000
 
     perm_betas = [False for _ in range(NUM_PERMUTATIONS)]
-
 
     data = import_data('data/data.csv')
     nrow = len(data['Y'])
@@ -39,9 +36,12 @@ if __name__ =='__main__':
     obs_beta = solve_linear_regression(data, response)
     key_beta = obs_beta[key_index]
 
+    response = response.as_matrix()
+
     for i in range(NUM_PERMUTATIONS):
+        print(i)
         permute_column(data, KEY)
-        perm_beta = solve_linear_regression(data, response)
+        perm_beta = solve_linear_regression(data.as_matrix(), response)
         perm_betas[i] = perm_beta[key_index]
 
 
@@ -49,8 +49,8 @@ if __name__ =='__main__':
     print(time2 - time1)
 
     if key_beta < 0:
-        p_value = sum([(b < obs_beta) or (b > -1*obs_beta) for b in perm_betas])/NUM_PERMUTATIONS
+        p_value = sum([(b < key_beta) or (b > -1*key_beta) for b in perm_betas])/NUM_PERMUTATIONS
     else :
-        p_value = sum([(b > obs_beta) or (b < -1*obs_beta) for b in perm_betas])/NUM_PERMUTATIONS
+        p_value = sum([(b > key_beta) or (b < -1*key_beta) for b in perm_betas])/NUM_PERMUTATIONS
 
     print(p_value)
